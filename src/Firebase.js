0639b88aea0740cffdc5fa3efe0fc211a03c6847
Virendra Kumar <100487078+Virendra-94@users.noreply.github.com>
2024-08-01 16:52:41 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,22 +1,16 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwp9V5aNBu5E0SsLH1y4qJrLL8QWUKGhM",
-  authDomain: "clone-6abe8.firebaseapp.com",
-  projectId: "clone-6abe8",
-  storageBucket: "clone-6abe8.appspot.com",
-  messagingSenderId: "752575321294",
-  appId: "1:752575321294:web:061c2c5c5aeed11253ec9f"
+  apiKey: "AIzaSyBuwga753JGu-JOAiXFDzro6rJexRse9Tw",
+  authDomain: "clone-af652.firebaseapp.com",
+  databaseURL: "https://clone-af652-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "clone-af652",
+  storageBucket: "clone-af652.appspot.com",
+  messagingSenderId: "547158821615",
+  appId: "1:547158821615:web:5787e71b697b5a9e41691a",
+  measurementId: "G-857NQSVH7N"
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBuwga753JGu-JOAiXFDzro6rJexRse9Tw",
-//   authDomain: "clone-af652.firebaseapp.com",
-//   projectId: "clone-af652",
-//   storageBucket: "clone-af652.appspot.com",
-//   messagingSenderId: "547158821615",
-//   appId: "1:547158821615:web:5787e71b697b5a9e41691a",
-//   measurementId: "G-857NQSVH7N"
-// };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
